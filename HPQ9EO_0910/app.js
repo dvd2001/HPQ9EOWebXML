5f7f5a8049ef01a910a96468c2997ef73b7c589f
dvd2001 <bedavid2001@gmail.com>
2025-09-13 16:37:50 +0200

    Adding content to the pages

--- HPQ9EO_0910/app.js
+++ HPQ9EO_0910/app.js
@@ -7,4 +7,14 @@ function loadNavigationBar() {
         }).catch(error => console.error('Error loading navigation bar:', error));
 }
 
+function loadFooter() {
+    fetch('foot.html')
+        .then(response => response.text())
+        .then(footerHtml => {
+            const htmlElement = document.querySelector('html');
+            htmlElement.insertAdjacentHTML('beforeend', footerHtml);
+        }).catch(error => console.error('Error loading footer:', error));
+}
+
 loadNavigationBar();
+loadFooter();
