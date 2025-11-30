1cc965b8a494bf3b97107daa2c4870645014e48d
dvd2001 <bedavid2001@gmail.com>
2025-11-30 17:21:59 +0100

    beadandó domWrite fájl átírása, hogy megfeleljen a feladatkiírásának

--- HPQ9EO_XMLTask/HPQ9EODOMParse/src/hpq9eo/domparse/hu/HPQ9EODOMWrite.java
+++ HPQ9EO_XMLTask/HPQ9EODOMParse/src/hpq9eo/domparse/hu/HPQ9EODOMWrite.java
@@ -8,137 +8,39 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
 
 import java.io.File;
 
 public class HPQ9EODOMWrite {
     public static void main(String[] args) {
         try {
-            // Új dokumentum létrehozása
+            // XML dokumentum betöltése
+            File xmlFile = new File("C:\\webadat\\HPQ9EOWebXML\\HPQ9EO_XMLTask\\1. feladat\\HPQ9EO_XML.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.newDocument();
+            Document doc = dBuilder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
 
-            // Gyökér elem létrehozása
-            Element rootElement = doc.createElement("Szerviz");
-            doc.appendChild(rootElement);
+            System.out.println("=== XML DOKUMENTUM TARTALMA - FA STRUKTÚRA ===");
+            System.out.println("=============================================");
 
-            // Tulajdonosok hozzáadása
-            addTulajdonos(doc, rootElement, "123456NA", "01", "Kovács János",
-                    "Budapest", "Kossuth Lajos utca", "10", "+363025815235");
-
-            addTulajdonos(doc, rootElement, "987654TA", "02", "Remek Elek",
-                    "Miskolc", "Kiraly utca", "5", "+36706584123");
-
-            // Szalonok hozzáadása
-            addSzalon(doc, rootElement, "01", "Budapest", "Fo utca", "1",
-                    "www.budapestauto.hu", new String[] { "Audi" });
-
-            addSzalon(doc, rootElement, "02", "Miskolc", "Derine utca", "7",
-                    "www.miskolcauto.hu", new String[] { "Mercedes", "Ford" });
-
-            // Autók hozzáadása
-            addAuto(doc, rootElement, "ABC-123", "01", "Audi", "A3", "Dízel");
-            addAuto(doc, rootElement, "CBA-321", "02", "Ford", "Fiesta", "Benzin");
-
-            // Dokumentum kiírása konzolra fa struktúrában
+            // Dokumentum tartalmának kiírása fa struktúrában a konzolra
             printDocumentTree(doc.getDocumentElement(), 0);
 
-            // Dokumentum mentése fájlba
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("HPQ9EO1XML.xml"));
-            transformer.transform(source, result);
-
-            System.out.println("\nXML fájl mentve: HPQ9EO1XML.xml");
+            // Dokumentum tartalmának mentése új XML fájlba
+            saveDocumentToFile(doc, "HPQ9EO1XML.xml");
+            System.out.println("\n=============================================");
+            System.out.println("Dokumentum sikeresen elmentve: HPQ9EO1XML.xml");
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void addTulajdonos(Document doc, Element root, String szemelyiSzam, String t_sz,
-            String nev, String varos, String utca, String hazszam, String telefonszam) {
-        Element tulajdonos = doc.createElement("Tulajdonos");
-        tulajdonos.setAttribute("szemelyiSzam", szemelyiSzam);
-        tulajdonos.setAttribute("t_sz", t_sz);
-
-        Element nevElem = doc.createElement("Nev");
-        nevElem.appendChild(doc.createTextNode(nev));
-        tulajdonos.appendChild(nevElem);
-
-        Element cim = doc.createElement("Cim");
-        addCimElements(doc, cim, varos, utca, hazszam);
-        tulajdonos.appendChild(cim);
-
-        Element tel = doc.createElement("Telefonszam");
-        tel.appendChild(doc.createTextNode(telefonszam));
-        tulajdonos.appendChild(tel);
-
-        root.appendChild(tulajdonos);
-    }
-
-    private static void addSzalon(Document doc, Element root, String id, String varos,
-            String utca, String hazszam, String weboldal, String[] markak) {
-        Element szalon = doc.createElement("Szalon");
-        szalon.setAttribute("id", id);
-
-        Element cim = doc.createElement("Cim");
-        addCimElements(doc, cim, varos, utca, hazszam);
-        szalon.appendChild(cim);
-
-        Element web = doc.createElement("Weboldal");
-        web.appendChild(doc.createTextNode(weboldal));
-        szalon.appendChild(web);
-
-        for (String marka : markak) {
-            Element markaElem = doc.createElement("Markaszerviz");
-            markaElem.appendChild(doc.createTextNode(marka));
-            szalon.appendChild(markaElem);
-        }
-
-        root.appendChild(szalon);
-    }
-
-    private static void addAuto(Document doc, Element root, String rendszam, String a_sz,
-            String marka, String tipus, String uzemanyag) {
-        Element auto = doc.createElement("Auto");
-        auto.setAttribute("rendszam", rendszam);
-        auto.setAttribute("a_sz", a_sz);
-
-        Element markaElem = doc.createElement("Marka");
-        markaElem.appendChild(doc.createTextNode(marka));
-        auto.appendChild(markaElem);
-
-        Element tipusElem = doc.createElement("Tipus");
-        tipusElem.appendChild(doc.createTextNode(tipus));
-        auto.appendChild(tipusElem);
-
-        Element uzemanyagElem = doc.createElement("Uzemanyag");
-        uzemanyagElem.appendChild(doc.createTextNode(uzemanyag));
-        auto.appendChild(uzemanyagElem);
-
-        root.appendChild(auto);
-    }
-
-    private static void addCimElements(Document doc, Element cim, String varos, String utca, String hazszam) {
-        Element varosElem = doc.createElement("Varos");
-        varosElem.appendChild(doc.createTextNode(varos));
-        cim.appendChild(varosElem);
-
-        Element utcaElem = doc.createElement("Utca");
-        utcaElem.appendChild(doc.createTextNode(utca));
-        cim.appendChild(utcaElem);
-
-        Element hazszamElem = doc.createElement("Hazszam");
-        hazszamElem.appendChild(doc.createTextNode(hazszam));
-        cim.appendChild(hazszamElem);
-    }
-
+    // Rekurzív metódus a dokumentum fa struktúrájának kiírására
     private static void printDocumentTree(Node node, int depth) {
         if (node.getNodeType() == Node.ELEMENT_NODE) {
             // Behúzás a mélység alapján
@@ -147,41 +49,85 @@ public class HPQ9EODOMWrite {
                 indent.append("  ");
             }
 
-            System.out.println(indent + "<" + node.getNodeName() + getAttributes(node) + ">");
+            // Elem nyitó tagje attribútumokkal
+            Element element = (Element) node;
+            System.out.println(indent + "<" + element.getNodeName() + getAttributes(element) + ">");
 
-            // Szöveges tartalom kiírása, ha van
-            if (node.getTextContent() != null && !node.getTextContent().trim().isEmpty() &&
-                    node.getChildNodes().getLength() == 1) {
-                System.out.println(indent + "  " + node.getTextContent().trim());
+            // Szöveges tartalom ellenőrzése
+            boolean hasElementChildren = hasElementChildren(element);
+            String textContent = element.getTextContent().trim();
+
+            // Ha van szöveges tartalom és nincs element gyermek, kiírjuk
+            if (!textContent.isEmpty() && !hasElementChildren) {
+                System.out.println(indent + "  " + textContent);
             }
 
-            // Gyermekek feldolgozása
-            NodeList children = node.getChildNodes();
+            // Gyermek elemek feldolgozása
+            NodeList children = element.getChildNodes();
             for (int i = 0; i < children.getLength(); i++) {
                 printDocumentTree(children.item(i), depth + 1);
             }
 
-            System.out.println(indent + "</" + node.getNodeName() + ">");
-        } else if (node.getNodeType() == Node.TEXT_NODE &&
-                node.getTextContent() != null && !node.getTextContent().trim().isEmpty()) {
-            // Szöveges tartalom kiírása
-            StringBuilder indent = new StringBuilder();
-            for (int i = 0; i < depth; i++) {
-                indent.append("  ");
+            // Elem záró tagje (csak ha van element gyermek vagy a szöveg nem üres)
+            if (hasElementChildren || (!textContent.isEmpty() && hasElementChildren)) {
+                System.out.println(indent + "</" + element.getNodeName() + ">");
             }
-            System.out.println(indent + node.getTextContent().trim());
+
+        } else if (node.getNodeType() == Node.TEXT_NODE) {
+            // Szöveges tartalom kiírása, ha nem üres
+            String text = node.getTextContent().trim();
+            if (!text.isEmpty()) {
+                StringBuilder indent = new StringBuilder();
+                for (int i = 0; i < depth; i++) {
+                    indent.append("  ");
+                }
+                System.out.println(indent + text);
+            }
         }
     }
 
-    private static String getAttributes(Node node) {
+    // Ellenőrzi, hogy egy elemnek van-e element típusú gyermeke
+    private static boolean hasElementChildren(Element element) {
+        NodeList children = element.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            if (children.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Az elem attribútumainak összegyűjtése string formátumba
+    private static String getAttributes(Element element) {
         StringBuilder attributes = new StringBuilder();
-        if (node.hasAttributes()) {
-            for (int i = 0; i < node.getAttributes().getLength(); i++) {
-                Node attr = node.getAttributes().item(i);
-                attributes.append(" ").append(attr.getNodeName()).append("=\"").append(attr.getNodeValue())
-                        .append("\"");
+        if (element.hasAttributes()) {
+            for (int i = 0; i < element.getAttributes().getLength(); i++) {
+                Node attr = element.getAttributes().item(i);
+                attributes.append(" ").append(attr.getNodeName())
+                        .append("=\"").append(attr.getNodeValue()).append("\"");
             }
         }
         return attributes.toString();
+    }
+
+    // Dokumentum mentése fájlba
+    private static void saveDocumentToFile(Document doc, String filename) {
+        try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            // Formázási beállítások
+            transformer.setOutputProperty(javax.xml.transform.OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(javax.xml.transform.OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(filename));
+            transformer.transform(source, result);
+
+        } catch (Exception e) {
+            System.err.println("Hiba a fájl mentése során: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
